8acc3d27b035054b0c938ca5630241e4c470d647
Hojinjja <thunder2001@naver.com>
2023-10-11 14:01:33 +0900

    로그인

--- src/main/java/Opensource/SharingService/repository/MemberRepository.java
+++ src/main/java/Opensource/SharingService/repository/MemberRepository.java
@@ -1,15 +1,18 @@
 package Opensource.SharingService.repository;
 
-import Opensource.SharingService.entity.UserEntity;
+import Opensource.SharingService.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< HEAD
-import java.util.List;
+//<<<<<<< HEAD
 import java.util.Optional;
 
 
-public interface UserRepository {
-    User save(UserEntity user);
+public interface MemberRepository extends JpaRepository<MemberEntity, Long>{
+    //이메일로 회원 정보 조회 (selet*from member_table where member_email=?)
+    Optional<MemberEntity>findByMemberEmail(String UserEmail);
+}
+
+/*    User save(UserEntity user);
 //    Optional<User> findByName(String name);
     Optional<UserEntity> findById(Long id);
     List<UserEntity> finAll();
@@ -18,3 +21,5 @@ public interface UserRepository {
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 >>>>>>> 09dffda9f9909a83096b88aa0d1dbcedbddf6485
 }
+*/
+
