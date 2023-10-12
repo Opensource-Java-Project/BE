b45d4cad717ccfee7c03a0bc31473f3858773109
ChanY00 <csdud77@naver.com>
2023-10-12 12:34:31 +0900

    게시판

--- src/main/java/Opensource/SharingService/dto/BoardDTO.java
+++ src/main/java/Opensource/SharingService/dto/BoardDTO.java
@@ -1,6 +1,8 @@
 package Opensource.SharingService.dto;
 
+import Opensource.SharingService.entity.BoardEntity;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
@@ -19,12 +21,34 @@ public class BoardDTO {
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdatedTime;
 
+    private MultipartFile boardFile; // save.html -> Controller 파일담는용도
+    private String originalFileName;
+    private String storedFileName;
+    private int fileAttached;
+
     public BoardDTO(Long id, String boardWriter, String boardTitle, String boardContents, int boardHits, LocalDateTime boardCreatedTime){
         this.id = id;
         this.boardWriter = boardWriter;
         this.boardTitle = boardTitle;
         this.boardHits = boardHits;
         this.boardCreatedTime = boardCreatedTime;
+    }
+
+    public static BoardDTO toBoardDTO (BoardEntity boardEntity) {
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardPass(boardEntity.getBoardPass());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardHits(boardEntity.getBoardHits());
+        boardDTO.setBoardCreatedTime(boardEntity.getCreatedTime());
+        boardDTO.setBoardUpdatedTime(boardEntity.getUpdatedTime());
+        if (boardEntity.getFileAttached() == 0) {
+            boardDTO.setFileAttached(boardEntity.getFileAttached());
+        } else {
+            boardDTO.setFileAttached(boardEntity.getFileAttached());
+
+        }
     }
 
 
