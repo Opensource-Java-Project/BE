ec8637862a75be3b383160ff75e9849dfff2a649
ChanY00 <csdud77@naver.com>
2023-11-22 13:02:08 +0900

    수정

--- src/main/java/Opensource/SharingService/repository/CommentRepository.java
+++ src/main/java/Opensource/SharingService/repository/CommentRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
 
-    List<CommentEntity> findAllByBoardEntityOrderByIndexDesc(BoardEntity boardEntity);
+    List<CommentEntity> findAllByBoardEntityOrderByCommentIndexDesc(BoardEntity boardEntity);
 }
