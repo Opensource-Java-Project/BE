b572b4fca714a64ef99458fb0bb5b6a356c00472
ChanY00 <csdud77@naver.com>
2023-11-28 15:22:51 +0900

    수정

--- src/main/java/Opensource/SharingService/config/WebConfig.java
+++ src/main/java/Opensource/SharingService/config/WebConfig.java
@@ -1,8 +1,11 @@
 package Opensource.SharingService.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     private String resourcePath = "/upload/**";
     private String savePath = "file:/c:/springboot_img/";
@@ -11,5 +14,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(resourcePath)
                 .addResourceLocations(savePath);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
